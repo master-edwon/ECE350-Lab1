53ce323909fa4d02ae6ead6c49e9ab05b84b8614
Edwin Chung <144295747+master-edwon@users.noreply.github.com>
2025-12-12 00:42:35 -0500

    Refactor compute_c to use ap_fixed data type

--- compute_tb.cpp
+++ compute_tb.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
+#include "ap_fixed.h"
 
-void compute_c(double a, double b, double &c);
+typedef ap_fixed<16, 8> data_t;
+
+void compute_c(data_t a, data_t b, data_t &c);
 
 int main() {
-    double a, b, c;
+    data_t a, b, c;
 
+    // Test Case: 3-4-5 Triangle
     a = 3;
     b = 4;
     compute_c(a, b, c);
+    
     std::cout << "a=" << a << ", b=" << b << " => c=" << c << std::endl;
 
-
+    // Test Case: 5-12-13 Triangle
     a = 12;
     b = 5;
     compute_c(a, b, c);
     std::cout << "a=" << a << ", b=" << b << " => c=" << c << std::endl;
-
 
     return 0;
 }
